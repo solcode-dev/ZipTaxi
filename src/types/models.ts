0e4944ce9a4e80de5c32d980239eff834dde0a03
Thor0111 <buddhaandai@gmail.com>
2026-01-22 20:34:50 +0900

    feat: readme 수정

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,3 +1,12 @@
+# firebase firestore에 저장되거나 앱 내부에서 주고받을 데이터 구조를 정의
+
+export interface User {
+   uid: string;
+   email: string;
+   createAt: Date;
+   displayName?: string;
+}
+
 export interface Revenue {
   id: string;
   userId: string;
